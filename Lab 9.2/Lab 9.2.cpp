03a3b724387350e19fe43f4730b301b6ce6953a2
Zachariah Schmunk <z-schmunk@onu.edu>
2024-11-07 00:51:00 -0500

    isSortedIncreasing

---
+++ Lab 9.2/Lab 9.2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+bool isSortedIncreasing(int arr[], int size) {
+    for (int i = 1; i < size; i++) {
+        if (arr[i] < arr[i - 1]) {
+            return false;
+        }
+    }
+    return true;
+}
