8b0bb2ee051d0af8848b3efbd14acd5738cb06ed
Zachariah Schmunk <z-schmunk@onu.edu>
2024-11-07 00:56:31 -0500

    completed hasDuplicates

--- Lab 9.2/Lab 9.2.cpp
+++ Lab 9.2/Lab 9.2.cpp
@@ -4,6 +4,7 @@ using namespace std;
 int isSortedIncreasing;
 int isSortedDecreasing;
 int hasAdjacentDuplicates;
+int hasDuplicates;
 
 bool isSortedIncreasing(int arr[], int size) {
     for (int i = 1; i < size; i++) {
@@ -25,6 +26,16 @@ bool hasAdjacentDuplicates(int arr[], int size) {
     for (int i = 1; i < size; i++) {
         if (arr[i] == arr[i - 1]) {
             return true;
+        }
+    }
+    return false;
+}
+bool hasDuplicates(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        for (int j = i + 1; j < size; j++) {
+            if (arr[i] == arr[j]) {
+                return true;
+            }
         }
     }
     return false;
