d93ee72011656b3f23ac11eb4ead27365f4f2dc5
Zachariah Schmunk <z-schmunk@onu.edu>
2024-11-07 01:02:00 -0500

    Completed P9-2.

--- Lab 9.2/Lab 9.2.cpp
+++ Lab 9.2/Lab 9.2.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 using namespace std;
 
-int isSortedIncreasing;
-int isSortedDecreasing;
-int hasAdjacentDuplicates;
-int hasDuplicates;
+bool isSortedIncreasing(int arr[], int size);
+bool isSortedDecreasing(int arr[], int size)
+bool hasAdjacentDuplicates(int arr[], int size);
+bool hasDuplicates(int arr[], int size);
 
 bool isSortedIncreasing(int arr[], int size) {
     for (int i = 1; i < size; i++) {
@@ -39,4 +39,41 @@ bool hasDuplicates(int arr[], int size) {
         }
     }
     return false;
+}
+int main() {
+    const int SIZE = 6;
+    int arr[SIZE];
+
+    for (int i = 0; i < SIZE; i++) {
+        cout << "Enter integer #" << i << ": ";
+        cin >> arr[i];
+    }
+
+    if (isSortedIncreasing(arr, SIZE)) {
+        cout << "The data are increasing." << endl;
+    }
+    else {
+        cout << "The data are not increasing." << endl;
+    }
+
+    if (isSortedDecreasing(arr, SIZE)) {
+        cout << "The data are decreasing." << endl;
+    }
+    else {
+        cout << "The data are not decreasing." << endl;
+    }
+
+    if (hasAdjacentDuplicates(arr, SIZE)) {
+        cout << "The data has adjacent duplicates." << endl;
+    }
+    else {
+        cout << "The data does not have adjacent duplicates." << endl;
+    }
+
+    if (hasDuplicates(arr, SIZE)) {
+        cout << "The data has duplicates." << endl;
+    }
+    else {
+        cout << "The data does not have duplicates." << endl;
+    }
 }
